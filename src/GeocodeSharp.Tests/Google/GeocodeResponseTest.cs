636faf03f3a5ee36195d14c79011003ccdd06053
Brandon <brandoncuff@gmail.com>
2015-02-21 17:03:46 -0800

    Added test for status serialization

---
+++ src/GeocodeSharp.Tests/Google/GeocodeResponseTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using GeocodeSharp.Google;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GeocodeSharp.Tests.Google
+{
+    [TestClass]
+    public class GeocodeResponseTest
+    {
+        [TestMethod]
+        public void TestStatusSerialization()
+        {
+            var before = new GeocodeResponse
+            {
+                StatusText = "OK",
+            };
+            var json = JsonConvert.SerializeObject(before);
+            dynamic after = JObject.Parse(json);
+            Assert.AreEqual("OK", (string)after.status);
+            Assert.AreEqual(null, after.Status);
+        }
+    }
+}
