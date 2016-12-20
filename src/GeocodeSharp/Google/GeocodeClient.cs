2e045a0fcdb5978612bf82afbd74e87a6abfd38c
Marco Casamento <marco.casamento@hotmail.it>
2016-12-20 13:10:33 +0100

    added method to return raw json instead of GeocodeResponse graph; added version to project.json

--- src/GeocodeSharp/Google/GeocodeClient.cs
+++ src/GeocodeSharp/Google/GeocodeClient.cs
@@ -84,8 +84,25 @@ namespace GeocodeSharp.Google
         public async Task<GeocodeResponse> GeocodeAddress(string address, string region = null, ComponentFilter filter = null, string language = null)
         {
             var request = BuildRequest(address, region, language, filter);
+            var response = await DoRequestAsync(request);
+            return JsonConvert.DeserializeObject<GeocodeResponse>(response);
+        }
+
+        /// <summary>
+        /// Calls Google's geocode API with the specified address and optional region.
+        /// https://developers.google.com/maps/documentation/geocoding/#GeocodingRequests
+        /// </summary>
+        /// <param name="address">The street address that you want to geocode, in the format used by the national postal service of the country concerned. Additional address elements such as business names and unit, suite or floor numbers should be avoided.</param>
+        /// <param name="region">The region code, specified as a ccTLD ("top-level domain") two-character value. This parameter will only influence, not fully restrict, results from the geocoder.</param>
+        /// <param name="language"> The language in which to return results. Address components will all be returned in the same language, which is chosen from the first component. Should names not be available in the preferred language, the closest match will be used.</param>
+        /// <param name="filter">A component filter for which you wish to obtain a geocode. The component filter swill fully restrict the results from the geocoder. Only the results that match all the filters will be returned. Each address component can only be specified either in the address parameter or as a component filter, but not both. Doing so may result in ZERO_RESULTS.</param>
+        /// <returns>The geocode response.</returns>
+        public async Task<string> GeocodeAddressJson(string address, string region = null, ComponentFilter filter = null, string language = null)
+        {
+            var request = BuildRequest(address, region, language, filter);
             return await DoRequestAsync(request);
         }
+
 
         /// <summary>
         /// Calls Google's geocode API with the specified address and optional region.
@@ -97,10 +114,24 @@ namespace GeocodeSharp.Google
         public async Task<GeocodeResponse> GeocodeComponentFilter(ComponentFilter filter, string region = null)
         {
             var request = BuildRequest(filter, region);
-            return await DoRequestAsync(request);
+            var response = await DoRequestAsync(request);
+            return JsonConvert.DeserializeObject<GeocodeResponse>(response);
         }
 
-        private async Task<GeocodeResponse> DoRequestAsync(HttpWebRequest request)
+        /// <summary>
+        /// Calls Google's geocode API with the specified address and optional region.
+        /// https://developers.google.com/maps/documentation/geocoding/#GeocodingRequests
+        /// </summary>
+        /// <param name="filter">A component filter for which you wish to obtain a geocode. The component filter swill fully restrict the results from the geocoder. Only the results that match all the filters will be returned. Each address component can only be specified either in the address parameter or as a component filter, but not both. Doing so may result in ZERO_RESULTS.</param>
+        /// <param name="region">The region code, specified as a ccTLD ("top-level domain") two-character value. This parameter will only influence, not fully restrict, results from the geocoder.</param>
+        /// <returns>The geocode response.</returns>
+        public async Task<string> GeocodeComponentFilterJson(ComponentFilter filter, string region = null)
+        {
+            var request = BuildRequest(filter, region);
+            return await DoRequestAsync(request);            
+        }
+
+        private async Task<string> DoRequestAsync(HttpWebRequest request)
         {
             string json;
             using (var ms = new MemoryStream())
@@ -114,10 +145,10 @@ namespace GeocodeSharp.Google
 
                 json = Encoding.UTF8.GetString(ms.ToArray());
             }
-
-            return JsonConvert.DeserializeObject<GeocodeResponse>(json);
+            return json;
         }
 
+        
         private HttpWebRequest BuildRequest(ComponentFilter filter, string region)
         {
             if (filter == null)
